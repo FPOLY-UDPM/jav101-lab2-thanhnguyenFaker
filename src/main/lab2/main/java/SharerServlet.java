a216f579ea95d50b5818ca1c1df84582498c3456
thanh <thanhnguyen10072004@gmail.com>
2026-05-27 11:09:46 +0700

    Nộp lab 2

--- src/main/lab2/main/java/SharerServlet.java
+++ src/main/lab2/main/java/SharerServlet.java
@@ -3,7 +3,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javasrc.io.IOException;
 import java.util.Date;
 
 @WebServlet("/bai1.1")
